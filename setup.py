851612e90c8f750620c870eca3f2527d3c69a402
jvadair <dev@jvadair.com>
2022-12-20 16:37:10 -0500

    Added README to PyPI

--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
 from setuptools import setup
 
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 setup(
     name='python-screen',
-    version='1.0.2',
+    version='1.0.3',
     packages=['pyscreen'],
     url='https://gitlab.com/jvadair/pyscreen',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     license='Apache 2.0',
     author='jvadair',
     author_email='dev@jvadair.com',
